ffcb201de8cc2593b015aa35a3432a156b50c30b
Brenna Pavlinchak <littlemeg27@student.fullsail.edu>
2024-11-03 23:43:43 -0500

    Finished Lab1

--- Lab1/Lab1/Lab1/ViewController.swift
+++ Lab1/Lab1/Lab1/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController
 {
-
+    var movies = [Movies]()
+    var currentIndex = 0
+    
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var movieGenreLabel: UILabel!
+    @IBOutlet weak var movieYearLabel: UILabel!
+    @IBOutlet weak var movieActorsLabel: UILabel!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        displayMovie(at: currentIndex)
         
         if let path = Bundle.main.path(forResource: "Movies", ofType: ".json")
         {
@@ -37,7 +45,17 @@ class ViewController: UIViewController
             
         }
     }
-
+    
+    func displayMovie(at index: Int)
+    {
+        guard index >= 0 && index < movies.count else { return }
+        let movies = movies[index]
+        movieTitleLabel.text = movies.title
+        movieGenreLabel.text = movies.genre
+        movieYearLabel.text = movies.year
+        movieActorsLabel.text = movies.actors.joined(separator:  ", ")
+    }
+    
     func ParseJSON(jsonObject: [Any]?)
     {
         if let jsonObject = jsonObject
@@ -45,17 +63,31 @@ class ViewController: UIViewController
             for firstItem in jsonObject
             {
                 guard let object = firstItem as? [String: Any],
-                let movieTitle = object["title"] as? String,
-                let movieYear = object["year"] as? String,
-                let movieGenre = object["genre"] as? String,
-                let movieActors = object["actors"] as? [String]
-                        else { continue }
+                      let movieTitle = object["title"] as? String,
+                      let movieYear = object["year"] as? String,
+                      let movieGenre = object["genre"] as? String,
+                      let movieActors = object["actors"] as? [String]
+                else { continue }
                 
-                movie.append(Movies(title: movieTitle, genre: movieGenre, year: movieYear, actors: movieActors))
+                movies.append(Movies(title: movieTitle, genre: movieGenre, year: movieYear, actors: movieActors))
             }
         }
     }
     
-
+    @IBAction func nextMovie(_ sender: UIButton)
+    {
+        if currentIndex < movies.count - 1 {
+            currentIndex += 1
+            displayMovie(at: currentIndex)
+        }
+    }
+    
+    @IBAction func previousMovie(_ sender: UIButton)
+    {
+        if currentIndex > 0 {
+            currentIndex -= 1
+            displayMovie(at: currentIndex)
+        }
+    }
 }
 
